1a6e74b3706b0eb7fd8ae2454a2cf7d80db8a447
2025657-pixel <2025657@student.cct.ie>
2025-12-11 12:34:10 -0800

    histogram

--- uber.py
+++ uber.py
@@ -21,3 +21,9 @@ data_load_state.text("Done!")
 if st.checkbox("Show Raw Data"):
     st.subheader("Raw Data")
     st.write((data))
+
+
+st.subheader("Histogram:Number of pickups per Hour")
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour,
+                           bins=24, range=(0.24))[0]
+st.bar_chart(hist_values)
